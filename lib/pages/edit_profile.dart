7ed131b3df9cc97f4d1743b4ced5281b3f1b56c1
jamir <jamir@live.jp>
2020-09-07 20:17:40 -0300

    create edit profile screen, and profile screen

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,13 +1,156 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
+import 'package:savepoint/models/user.dart';
+import 'package:savepoint/pages/home.dart';
+import 'package:savepoint/widgets/progress.dart';
 
 class EditProfile extends StatefulWidget {
+  final String currentUserId;
+  EditProfile({this.currentUserId});
+
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  bool isLoading = false;
+  User user;
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  getUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    DocumentSnapshot doc = await usersRef.document(widget.currentUserId).get();
+    user = User.fromDocument(doc);
+    displayNameController.text = user.displayName;
+    bioController.text = user.bio;
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Column buildDisplayNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12),
+          child: Text(
+            "Display Name",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: displayNameController,
+          decoration: InputDecoration(hintText: "Udpate Display Name"),
+        ),
+      ],
+    );
+  }
+
+  Column buildBioField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12),
+          child: Text(
+            "Bio",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: bioController,
+          decoration: InputDecoration(hintText: "Udpate your bio"),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Edit Profile');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Edit profile",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(
+                Icons.done,
+                size: 30,
+                color: Colors.green,
+              )),
+        ],
+      ),
+      body: isLoading
+          ? circularProgress()
+          : ListView(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 16,
+                          bottom: 8,
+                        ),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              CachedNetworkImageProvider(user.avatar),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          children: <Widget>[
+                            buildDisplayNameField(),
+                            buildBioField(),
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () => print("update profile"),
+                        child: Text(
+                          "Update Profile",
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: FlatButton.icon(
+                          onPressed: () => print("logout"),
+                          icon: Icon(Icons.cancel, color: Colors.red),
+                          label: Text(
+                            "Logout",
+                            style: TextStyle(color: Colors.red, fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+    );
   }
 }
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,18 +1,180 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:savepoint/models/user.dart';
+import 'package:savepoint/pages/home.dart';
+import 'package:savepoint/widgets/progress.dart';
 
 import '../widgets/header.dart';
+import 'edit_profile.dart';
 
 class Profile extends StatefulWidget {
+  final String profileId;
+  Profile({this.profileId});
+
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser?.id;
+
+  buildProfileHeader() {
+    Column buildCountColumn(String label, int count) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            count.toString(),
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 4),
+            child: Text(
+              label,
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+        ],
+      );
+    }
+
+    editProfile() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => EditProfile(currentUserId: currentUserId),
+        ),
+      );
+    }
+
+    buildButton({String text, Function function}) {
+      return Container(
+        padding: EdgeInsets.only(top: 2),
+        child: FlatButton(
+          onPressed: function,
+          child: Container(
+            width: 250,
+            height: 27,
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(
+                color: Colors.blue,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+      );
+    }
+
+    buildProfileButton() {
+//view own profile, sho profile button to edit
+      bool isProfileOwner = currentUserId == widget.profileId;
+      if (isProfileOwner) {
+        return buildButton(
+          text: "Edit profile",
+          function: editProfile,
+        );
+      }
+    }
+
+    return FutureBuilder(
+        future: usersRef.document(widget.profileId).get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          User user = User.fromDocument(snapshot.data);
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: CachedNetworkImageProvider(user.avatar),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              buildCountColumn("posts", 0),
+                              buildCountColumn("followers", 0),
+                              buildCountColumn("following", 0),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildProfileButton(),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(top: 12),
+                  child: Text(
+                    user.displayName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(top: 4),
+                  child: Text(
+                    "@" + user.username,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text(
+                    user.bio,
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: "profile"),
-      body: Text("Profile"),
+      body: ListView(
+        children: <Widget>[
+          buildProfileHeader(),
+        ],
+      ),
     );
   }
 }
