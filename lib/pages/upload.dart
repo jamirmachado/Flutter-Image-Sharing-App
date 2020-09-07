2651888fc2187e2f074c0e1891836ac2c9384af3
jamir <jamir@live.jp>
2020-09-07 19:05:29 -0300

    get user location

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:savepoint/models/user.dart';
@@ -263,7 +264,7 @@ class _UploadState extends State<Upload> {
                 borderRadius: BorderRadius.circular(30),
               ),
               color: Colors.blue,
-              onPressed: () => print("get user location"),
+              onPressed: getUserLocation,
               icon: Icon(
                 Icons.my_location,
                 color: Colors.white,
@@ -273,6 +274,16 @@ class _UploadState extends State<Upload> {
         ],
       ),
     );
+  }
+
+  getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+    String formattedAddress = "${placemark.locality}, ${placemark.country}";
+    locationController.text = formattedAddress;
   }
 
   @override
