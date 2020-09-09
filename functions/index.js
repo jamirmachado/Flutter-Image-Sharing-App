a7b6bb7b3bd8573cfa9facdfa60ce48fcf5b8ba0
jamir <jamir@live.jp>
2020-09-09 01:58:25 -0300

    create timeline on users and delete timeline

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+//const { user } = require('firebase-functions/lib/providers/auth');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,60 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.onCreateFollower = functions.firestore
+.document('/followers/{userId}/userFollowers/{followerId}')
+.onCreate(async (snapshot, context) => {
+    console.log("Follower created", snapshot.id);
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    // create followed users posts
+    const followedUserPostsRef = admin
+    .firestore()
+    .collection('posts')
+    .doc(userId)
+    .collection('userPosts');
+
+    //create following users timeline
+    const timelinePostsRef = admin
+    .firestore()
+    .collection('timeline')
+    .doc(followerId)
+    .collection('timelinePosts');
+
+    //get followed users posts
+    const querySnapshot = await followedUserPostsRef.get();
+
+    //add each user post to following users timeline
+
+    querySnapshot.forEach(doc => {
+        if(doc.exists) {
+            const postId = doc.id;
+            const postData = doc.data();
+            timelinePostsRef.doc(postId).set(postData);
+        }
+    });
+});
+
+exports.onDeleteFollower = functions.firestore
+.document('/followers/{userId}/userFollowers/{followerId}')
+.onDelete(async (snapshot, context) => {
+    console.log('Follower deleted', snapshot.id);
+
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    const timelinePostsRef = admin
+    .firestore()
+    .collection('timeline')
+    .doc(followerId)
+    .collection('timelinePosts')
+    .where('ownerId', "==", userId);
+
+    const querySnapshot = await timelinePostsRef.get();
+    querySnapshot.forEach(doc => {
+        if(doc.exists) {
+            doc.ref.delete();
+        }
+    });
+});
